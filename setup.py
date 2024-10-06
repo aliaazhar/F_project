bd4f0734574729364e62eb4743053988f02881ba
Azhar Ali <azhrali2018mp@gmail.com>
2024-10-06 19:13:51 +0530

    Fix requirements.txt file

--- setup.py
+++ setup.py
@@ -1,24 +1,49 @@
-from setuptools import find_packages,setup
+# from setuptools import find_packages,setup
+# from typing import List
+
+# HYPHEN_E_DOT = '-e .'
+# def get_requirements(file_path:str)->list[str]:
+#     """Returns a list of requirements from a requirements.txt file."""
+#     requirements=[]
+#     with open(file_path) as file_obj:
+#         requirements=file_obj.readlines()
+#         requirements= [req.replace("\n","") for req in requirments] 
+
+#         if HYPHEN_E_DOT in requirements:
+#             requirements.remove(HYPHEN_E_DOT)
+
+#         return requirements
+# setup(
+#     name='my_package',
+#     version='1.0.0',
+#     author='Azhar Ali',
+#     author_email='azharali@gmail.com',
+#     packages=find_packages(),   
+#     install_requires=get_requirements('requirements.txt')
+
+# )
+
+
+
+
+from setuptools import find_packages, setup
 from typing import List
 
 HYPHEN_E_DOT = '-e .'
-def get_requirment(file_path:str)->list[str]:
+
+def get_requirements(file_path: str) -> List[str]:
     """Returns a list of requirements from a requirements.txt file."""
-    requirments=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirments=file_obj.readlines()
-        requirments= [req.replace("\n","") for req in requirments] 
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() and req.strip() != HYPHEN_E_DOT]
+    return requirements
 
-        if HYPHEN_E_DOT in requirments:
-            requirments.remove(HYPHEN_E_DOT)
-
-        return requirments
 setup(
     name='my_package',
     version='1.0.0',
     author='Azhar Ali',
     author_email='azharali@gmail.com',
-    packages=find_packages(),   
-    install_requires=get_requirment('requirmrnts.txt')
-
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
